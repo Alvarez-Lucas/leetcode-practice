a51a52bb099772a9aa5cd2d816ac648b64ee4b9b
Lucas Alvarez <lucasalvarez@pm.me>
2022-05-31 22:22:34 -0400

    Add Valid Anagram Second Solution

--- 01_Arrays and Hashing/3_Valid Anagram (242).py
+++ 01_Arrays and Hashing/3_Valid Anagram (242).py
@@ -1,9 +1,13 @@
+from re import I
+
+
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
+    # My initial solution
+    def isAnagramInit(self, s: str, t: str) -> bool:
         """Given two strings s and t, return true if t is an anagram of s, and false otherwise."""
-        letters = {}
         if len(s) != len(t):
             return False
+        letters = {}
         for char in s:
             letters[char] = 1 if char not in letters else letters[char] + 1
         for char in t:
@@ -11,6 +15,19 @@ class Solution:
                 return False
             letters[char] -= 1
         return True
+
+    # Slightly modified source solution
+    def isAnagram(self, s: str, t: str) -> bool:
+        """Given two strings s and t, return true if t is an anagram of s, and false otherwise."""
+        if len(s) != len(t):
+            return False
+        countS, countT = {}, {}
+        for charS, charT in zip(s, t):
+            countS[charS] = 1 + countS.get(charS, 0)
+            countT[charT] = 1 + countS.get(charT, 0)
+        for key in countS:
+            if countS[key] != countT.get(key, 0):
+                return False
 
 
 def main():
