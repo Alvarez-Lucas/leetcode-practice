6ba122d1f2d4633b7467db20389a8c89c3ce79eb
Lucas Alvarez <lucasalvarez@pm.me>
2022-06-08 14:02:21 -0400

    Complete Two Sum II

--- 02_Two Pointers/2_Two Sum II (167).py
+++ 02_Two Pointers/2_Two Sum II (167).py
@@ -2,20 +2,30 @@ from typing import List
 
 
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    # init
+    def twoSumInit(self, numbers: List[int], target: int) -> List[int]:
         print(numbers)
         left_pointer = 0
         right_pointer = len(numbers) - 1
         while numbers[left_pointer] + numbers[right_pointer] != target:
             if numbers[left_pointer] + numbers[right_pointer] > target:
                 right_pointer -= 1
-                print("yah")
             elif numbers[left_pointer] + numbers[right_pointer] < target:
                 left_pointer += 1
-                print("has")
-            else:
-                print("asd")
         return [left_pointer + 1, right_pointer + 1]
+
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        print(numbers)
+        left_pointer = 0
+        right_pointer = len(numbers) - 1
+        while left_pointer < right_pointer:
+            current_sum = numbers[left_pointer] + numbers[right_pointer]
+            if current_sum > target:
+                right_pointer -= 1
+            elif current_sum < target:
+                left_pointer += 1
+            else:
+                return [left_pointer + 1, right_pointer + 1]
 
 
 def main():
